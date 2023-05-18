02e8dc2599b1135700c78d88fb3ddd3626e6e9bf
dmitriy.kranin <dmitriy.kranin@inventi.cz>
2023-05-18 19:11:02 +0200

    article redesigne

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,9 +1,10 @@
-import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text, TextAlign } from '@/shared/ui/deprecated/Text/Text';
+import { Text as TextDeprecated, TextAlign } from '@/shared/ui/deprecated/Text/Text';
 import cls from './ArticleImageBlockComponent.module.scss';
 import { ArticleImageBlock } from '../../model/types/article';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Text } from '@/shared/ui/redesigned/Text/Text';
 
 interface ArticleImageBlockComponentProps {
     className?: string;
@@ -13,19 +14,32 @@ interface ArticleImageBlockComponentProps {
 export const ArticleImageBlockComponent = memo(
     (props: ArticleImageBlockComponentProps) => {
         const { className, block } = props;
-        const { t } = useTranslation();
 
         return (
-            <div
-                className={classNames(cls.ArticleImageBlockComponent, {}, [
-                    className,
-                ])}
-            >
-                <img src={block.src} alt={block.title} className={cls.img} />
-                {block.title && (
-                    <Text text={block.title} align={TextAlign.CENTER} />
-                )}
-            </div>
+            <ToggleFeatures name='isAppRedesigned' on={
+                <div
+                    className={classNames(cls.ArticleImageBlockComponent, {}, [
+                        className,
+                    ])}
+                >
+                    <img src={block.src} alt={block.title} className={cls.img} />
+                    {block.title && (
+                        <Text TextVariant='accent' text={block.title} align='center' />
+                    )}
+                </div>
+            }
+                off={
+                    <div
+                        className={classNames(cls.ArticleImageBlockComponent, {}, [
+                            className,
+                        ])}
+                    >
+                        <img src={block.src} alt={block.title} className={cls.img} />
+                        {block.title && (
+                            <TextDeprecated text={block.title} align={TextAlign.CENTER} />
+                        )}
+                    </div>} />
+
         );
     },
 );
